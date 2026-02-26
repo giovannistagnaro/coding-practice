2162c2ecaaf498ce5599fcaca5bc894af6f4e70a
giovannistagnaro <giovannistagnaro20@gmail.com>
2026-02-26 12:51:01 -0700

    Small Linked List Cycle Detection update

--- problems/neetcode/blind75/Linked_List/Linked_List_Cycle_Detection/solution.py
+++ problems/neetcode/blind75/Linked_List/Linked_List_Cycle_Detection/solution.py
@@ -17,6 +17,6 @@ class Solution:
         while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
-            if slow.val == fast.val:
+            if slow == fast:
                 return True
         return False
