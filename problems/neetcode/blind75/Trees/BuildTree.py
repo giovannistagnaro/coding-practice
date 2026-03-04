a38633dd2e15da8c5be77d85cbdc0083f3904c81
giovannistagnaro <giovannistagnaro20@gmail.com>
2026-03-03 22:33:34 -0700

    Update to fix BuildTree behavior when nodes are None

--- problems/neetcode/blind75/Trees/BuildTree.py
+++ problems/neetcode/blind75/Trees/BuildTree.py
@@ -3,32 +3,33 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+from collections import deque
 from TreeNode import TreeNode
-from typing import List
+from typing import List, Optional
 
-def buildTree(nodes: List[int | None]) -> TreeNode:
+
+def buildTree(nodes: List[Optional[int]]) -> Optional[TreeNode]:
     if not nodes or nodes[0] is None:
         return None
-    
-    tree_nodes = [
-        TreeNode(val) if val is not None else None
-        for val in nodes
-    ]
-    n = len(tree_nodes)
 
-    for i in range(n):
-        if tree_nodes[i] is None:
-            continue
+    root = TreeNode(nodes[0])
+    queue = deque([root])
+    i = 1
 
-        left_i = 2 * i + 1
-        right_i = 2 * i + 2
+    while queue and i < len(nodes):
+        current = queue.popleft()
 
-        if left_i < n:
-            tree_nodes[i].left = tree_nodes[left_i]
-        if right_i < n:
-            tree_nodes[i].right = tree_nodes[right_i]
+        if i < len(nodes) and nodes[i] is not None:
+            current.left = TreeNode(nodes[i])
+            queue.append(current.left)
+        i += 1
 
-    return tree_nodes[0]
+        if i < len(nodes) and nodes[i] is not None:
+            current.right = TreeNode(nodes[i])
+            queue.append(current.right)
+        i += 1
+
+    return root
 
 def isEqual(tree1: TreeNode, tree2: TreeNode):
     return tree1 is tree2 if not tree1 or not tree2 else (
@@ -36,3 +37,13 @@ def isEqual(tree1: TreeNode, tree2: TreeNode):
         isEqual(tree1.left, tree2.left) and
         isEqual(tree1.right, tree2.right)
     )
+
+
+def printTree(root: TreeNode):
+    if not root:
+        return
+    if root.left:
+        printTree(root.left)
+    print(root.val)
+    if root.right:
+        printTree(root.right)
